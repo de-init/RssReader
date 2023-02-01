a155b61d3868aad6128d9adede4d452b48db03da
Vlad <snawyx@gmail.com>
2023-02-01 11:03:16 +0300

    Update gitignore & upload project

---
+++ RssReader/RssReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RssReader
+//
+//  Created by S2 on 1.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
